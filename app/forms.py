10dd97ff11dae1023654087c3e3dab84cb7e755e
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-24 18:21:07 -0500

    edit tasks

--- app/forms.py
+++ app/forms.py
@@ -13,5 +13,9 @@ class TodoForm(FlaskForm):
     description = StringField('Description', validators=[DataRequired()])
     submit = SubmitField('Create')
 
+
 class DeleteTodoForm(FlaskForm):
     submit = SubmitField('Delete')
+
+class UpdateTodoForm(FlaskForm):
+    submit = SubmitField('Update')
