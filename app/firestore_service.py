49427f6f7ffdebeca47a6a727695da99af4fdb8b
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-24 11:27:06 -0500

    firebase implement

---
+++ app/firestore_service.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("./to-do-635cd-firebase-adminsdk-c24ws-e424a54dda.json") 
+default_app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def get_users():
+    return db.collection('users').get()
+
+def get_todos(user_id):
+    return db.collection('users')\
+        .document(user_id)\
+        .collection('todos').get()
